83b6748419f01139c32cf816ffff5c8c14107ae5
robinson2810 <robinsonjjwin@gmail.com>
2026-01-22 03:09:59 +0530

    collections concepts

---
+++ src/language/basics/class2.java
@@ -0,0 +1,15 @@
+package language.basics;
+
+public class class2 extends class1{
+	String emp_name3 ="lala";
+public static void main(String[] args) {
+	class2 obj1=new class2();
+	class3 obj2=new class3();
+	System.out.println(obj1.emp_name);
+	System.out.println(obj1.emp_id);
+	System.out.println(obj2.emp_name1);
+	System.out.println(obj2.emp_id1);
+	System.out.println(obj1.emp_name3);
+}
+
+}
---
+++ src/language/basics/class3.java
@@ -0,0 +1,9 @@
+package language.basics;
+
+public class class3 {
+
+	String emp_name1 = "edwinson";
+	int emp_id1 = 12345;
+	
+	
+}
