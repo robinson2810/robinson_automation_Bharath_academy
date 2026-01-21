e3614d304fe17ac1a2a7406f18746e75fd1cafc9
robinson2810 <robinsonjjwin@gmail.com>
2026-01-22 03:21:50 +0530

    Add Java classes

---
+++ src/language/basics/class1.java
@@ -0,0 +1,19 @@
+package language.basics;
+
+public class class1 extends class3{
+	
+	String emp_name = "Robinson";
+	int emp_id = 1234;
+	public static void main(String[] args) {
+		class1 obj3=new class1();
+		System.out.println(obj3.emp_name);
+		System.out.println(obj3.emp_id);
+		System.out.println(obj3.emp_name1);
+		System.out.println(obj3.emp_id1);
+		
+		
+	}
+	
+
+}
+
